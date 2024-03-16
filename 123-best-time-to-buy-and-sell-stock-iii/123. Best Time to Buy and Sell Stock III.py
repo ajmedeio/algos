23409b5c0f678f1b8f13f6ce2d0836d83fb99cc1
Alexander Medeiros <ajmed13@gmail.com>
2024-03-16 09:00:01 -0400

    Runtime: 1021 ms (faster than 31.15%), Memory: 30.5 MB (less than 76.4%) - GitSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, P: List[int]) -> int:
+        k = 2
+        buy, sell = [inf] * (k+1), [0] * (k+1)
+        for i in range(len(P)):
+            for j in range(1, k+1):
+                buy[j] = min(buy[j], P[i] - sell[j-1])
+                sell[j] = max(sell[j], P[i] - buy[j])
+        return sell[k]
